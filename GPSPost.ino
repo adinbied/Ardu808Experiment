81e7f344c896380e679bfce47426ba229b11aa75
Adin Biederman <adinbied@gmail.com>
2018-04-20 13:12:29 -0700

    Add GPS Arduino Script & strip personal info

---
+++ GPSPost.ino
@@ -0,0 +1,97 @@
+//Bodged together by Adin Biederman, 2018
+//Credit to Adafruit for libraries and base code
+
+// Gets GPS Data and Posts it to Web Server
+#include "Adafruit_FONA.h"
+
+// standard pins for the shield, adjust as necessary
+#define FONA_RX 2
+#define FONA_TX 3
+#define FONA_RST 4
+
+// We default to using software serial. If you want to use hardware serial
+// (because softserial isnt supported) comment out the following three lines 
+// and uncomment the HardwareSerial line
+#include <SoftwareSerial.h>
+SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
+SoftwareSerial *fonaSerial = &fonaSS;
+
+// Hardware serial is also possible!
+//  HardwareSerial *fonaSerial = &Serial1;
+
+Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
+void setup() {
+
+  while (! Serial);
+
+  Serial.begin(115200);
+  Serial.println(F("Get GPS Data & SMS it to Number"));
+  Serial.println(F("Initializing FONA... (May take a few seconds)"));
+
+  fonaSerial->begin(4800);
+  if (! fona.begin(*fonaSerial)) {
+    Serial.println(F("Couldn't find FONA"));
+    while(1);
+  }
+  Serial.println(F("FONA is OK"));
+  // Try to enable GPRS
+  
+
+  Serial.println(F("Enabling GPS..."));
+  fona.enableGPS(true);
+  
+  // Print SIM card IMEI number.
+  char imei[16] = {0}; // MUST use a 16 character buffer for IMEI!
+  uint8_t imeiLen = fona.getIMEI(imei);
+  if (imeiLen > 0) {
+    Serial.print("SIM card IMEI: "); Serial.println(imei);
+  }
+        // turn GPRS on
+        if (!fona.enableGPRS(true)) {
+          Serial.println(F("Failed to turn on"));
+}
+}
+void loop() {
+  delay(8000);
+
+  float latitude, longitude, speed_kph, heading, speed_mph, altitude;
+
+  // if you ask for an altitude reading, getGPS will return false if there isn't a 3D fix
+  boolean gps_success = fona.getGPS(&latitude, &longitude, &speed_kph, &heading, &altitude);
+
+  if (gps_success) {
+    
+//Send back an automatic response
+char gpsdata[120];
+        fona.getGPS(0, gpsdata, 120);
+          Serial.println(F("Reply in format: mode,fixstatus,utctime(yyyymmddHHMMSS),latitude,longitude,altitude,speed,course,fixmode,reserved1,HDOP,PDOP,VDOP,reserved2,view_satellites,used_satellites,reserved3,C/N0max,HPA,VPA"));
+        Serial.println(gpsdata);
+      Serial.println("Sending Data...");
+       uint16_t statuscode;
+        int16_t length;
+        char url[80];
+        char data[80];
+
+        Serial.println(F("****"));
+        if (!fona.HTTP_POST_start("<ip address of webserver>", F("text/plain"), (uint8_t *) gpsdata, strlen(gpsdata), &statuscode, (uint16_t *)&length)) {
+          Serial.println("Failed!");
+        }
+        while (length > 0) {
+          while (fona.available()) {
+            char c = fona.read();
+
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
+            loop_until_bit_is_set(UCSR0A, UDRE0); /* Wait until data register empty. */
+            UDR0 = c;
+#else
+            Serial.write(c);
+#endif
+
+            length--;
+            if (! length) break;
+          }
+        }
+        Serial.println(F("\n****"));
+        fona.HTTP_POST_end();
+      }
+}
